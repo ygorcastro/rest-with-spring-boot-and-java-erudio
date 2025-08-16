68ad58fcc9f526dce4c91c9ef267fb52c8679a7b
ygor.castro <ygor.castro@outlook.com.br>
2025-08-16 06:54:10 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ src/main/java/com/br/ygorcastro/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.br.ygorcastro.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/com/br/ygorcastro/StartupTests.java
@@ -0,0 +1,13 @@
+package com.br.ygorcastro.rest_with_spring_boot_and_java_erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
